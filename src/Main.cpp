a26976cbfce8b30941562ed7312344bc1ab406e2
p2006809 <qarire@LAPTOP-U7PU27KU>
2023-10-13 14:22:59 +0200

    Modif main

--- src/Main.cpp
+++ src/Main.cpp
@@ -25,7 +25,7 @@ int main() {
 
     vector<Event*> events = {new Event(new ConditionTrue(), new ConsequenceTestDelete())};
 
-    field->Init(make_pair(50, 50));
+    field->Init(make_pair(1680, 1050));
     game->Init(); drawer->Init();
     updater->Init(new Behavior( events ));
 
@@ -38,6 +38,10 @@ int main() {
 //CharacterData(Team team, Stats stats, float healModifier, float damageModifier, Type type, Type favoriteEnemyType);
    
     ValueMaxed value;
+     vector<CharacterData*> tabcharact;
+    int playerTileX; 
+    int playerTileY; 
+
     value.Max = 100;
     value.Value = 30;
     Stats stat_perso;
@@ -45,44 +49,105 @@ int main() {
     stat_perso.def = value;
     stat_perso.atk = value;
     Team first_team = RED1;
-    Position pos = Position(Width/2,Height/2);
-    CharacterData* first_perso = new CharacterData(first_team,stat_perso,23,44,Melee,Tank);
-    first_perso->setPosition(pos);
+    Team seconde_team = Blue;
+
+    Position pos = Position(200,100);
+
+    CharacterData* new_r_perso = new CharacterData(first_team,stat_perso,23,44,Tank,Tank);
+    new_r_perso->setPosition(pos);
+    CharacterData* new_b_perso = new CharacterData(seconde_team,stat_perso,23,44,Melee,Melee);
+    new_b_perso->setPosition(pos);
+
+   
+   
     
-    field->addCharacterData(first_perso);
+    /// partie fond map
+    Color *pixels = (Color *)malloc(Width*Height*sizeof(Color));
 
-    vector<CharacterData*> tabcharact = field->getCharacterDataList();
+    for (int y = 0; y < Height; y++)
+    {
+        for (int x = 0; x < Width; x++)
+        {
+            if (((x/32+y/32)/1)%2 == 0) 
+                pixels[y*Width + x] = DARKGREEN;
+            else 
+                pixels[y*Width + x] = GREEN;
+        }
+    }
 
-    Position playerPosition = tabcharact[0]->getPosition();
-    int playerTileX = playerPosition.x;
-    int playerTileY = playerPosition.y;
+        Image checkedIm = {
+        .data = pixels,             // We can assign pixels directly to data
+        .width = Width,
+        .height = Height,
+        .mipmaps = 1,
+        .format = PIXELFORMAT_UNCOMPRESSED_R8G8B8A8
+        
+    };
 
-    RenderTexture2D fogOfWar = LoadRenderTexture(Height - 10, Width - 10);
-    SetTextureFilter(fogOfWar.texture, TEXTURE_FILTER_BILINEAR);
+    Texture2D checked = LoadTextureFromImage(checkedIm);
+    UnloadImage(checkedIm); 
+
+    /////
+
+    //RenderTexture2D fogOfWar = LoadRenderTexture(Height - 10, Width - 10);
+    //SetTextureFilter(fogOfWar.texture, TEXTURE_FILTER_BILINEAR);
     SetTargetFPS(60); 
 
     while (!WindowShouldClose()) { // Detect window close button or ESC key 
         
-        BeginTextureMode(fogOfWar);
-        EndTextureMode();
+        //BeginTextureMode(fogOfWar);
+        //EndTextureMode();
 
+
+        if(IsKeyPressed(KEY_R)) {
+            field->addCharacterData(new_r_perso);
+            tabcharact = field->getCharacterDataList();  
+        }
+
+        if(IsKeyPressed(KEY_B)) {
+            field->addCharacterData(new_b_perso);
+            tabcharact = field->getCharacterDataList();     
+        }
 
         BeginDrawing();
-        //DrawRectangleV(playerPosition, (Vector2){ PLAYER_SIZE, PLAYER_SIZE }, RED);
-        DrawRectangle(playerTileX, playerTileY, PLAYER_SIZE, PLAYER_SIZE, RED);
-        DrawText(TextFormat("Current tile: [%i,%i]", playerTileX, playerTileY), 10, 10, 20, RAYWHITE);
-        EndDrawing();
-
         
+        ClearBackground(RAYWHITE);
+
+        DrawTexture(checked, Width/2 - checked.width/2, Height/2 - checked.height/2, Fade(WHITE, 0.5f));
+
+
+        for(int i=0;i<tabcharact.size();i++)
+        {
+            Position playerPosition = tabcharact[i]->getPosition();
+            Team equipe = tabcharact[i]->getTeam();
+            if(equipe == RED1) {
+                
+                playerTileX = playerPosition.x + i*100;
+                playerTileY = playerPosition.y;
+
+                DrawRectangle(playerTileX, playerTileY, PLAYER_SIZE, PLAYER_SIZE, RED);
+            }else
+            {
+                playerTileX = playerPosition.x + i*100;
+                playerTileY = Width - playerPosition.y ;
+
+                DrawRectangle(playerTileX, playerTileY, PLAYER_SIZE, PLAYER_SIZE, BLUE);
+            }
+            
+           
+            
+           // DrawText(TextFormat("Current tile: [%i,%i]", playerTileX, playerTileY), 10, 10, 20, RAYWHITE);
+        }
+        
+        EndDrawing(); 
     }   
 
-    UnloadRenderTexture(fogOfWar);  // Unload render texture
-
-    CloseWindow();          // Close window and OpenGL context
+   
+    UnloadTexture(checked); 
+    CloseWindow();         
     //--------------------------------------------------------------------------------------
 
     return 0;
-
 
 
 // // Game loop
