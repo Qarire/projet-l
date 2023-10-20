3ef906ba3a5e2cd94f842465a99e6c4a5a0730f3
p2006809 <qarire@LAPTOP-U7PU27KU>
2023-10-20 15:32:06 +0200

    Modif Main

--- src/Main.cpp
+++ src/Main.cpp
@@ -9,7 +9,7 @@
 
 #include "raylib.h"
 
-#define PLAYER_SIZE      16  
+#define PLAYER_SIZE      26  
 #include "ConsequenceTestDelete.h"
 #include "ConditionTrue.h"
 
@@ -34,23 +34,18 @@ int main() {
 
     vector<Event*> events = {new Event(new ConditionTrue(), new ConsequenceTestDelete())};
     
-    field->Init(make_pair(1680, 900));
+    field->Init(make_pair(50, 50));
     game->Init(); drawer->Init();
     updater->Init(new Behavior( events ));
 
     int Height = field->getHeight();
     int Width = field->getWidth();
 
-    int screenWidth = 1680;
-    int screenHeight = 900;
+    float screenWidth = 1680;
+    float screenHeight = 900;
     InitWindow(screenWidth, screenHeight, "Game Window");
     Texture2D texture = LoadTexture("img/Sprite.png");                                      
     
-   
-
-
-    
-
     ValueMaxed value;
     vector<CharacterData*> tabcharact;
     int playerTileX; 
@@ -65,27 +60,13 @@ int main() {
     Team first_team = RED1;
     Team seconde_team = Blue;
 
-    Position pos = Position();
-
-    CharacterData* new_r_perso = new CharacterData(first_team,stat_perso,23,44,Tank,Tank);
-    //new_r_perso->setPosition(pos);
    
-    
-    
     pair<bool, CharacterData*> verif_moov;
-   
-
 
     Vector2 My_Mouse;
        
-    int width35 = Width * 0.35;
-    int width25 = Width * 0.25;
-
-    /*int PosXPink = width35;
-    int PosXGreen = width35 + width25;
-    int PosXRed = (width35 + width25) + width25;
-    int PosXGray = ((width35 + width25) + width25) + width25;
-    int PosXBlue = (((width35 + width25) + width25) + width25) + width25;*/
+    int width35 = screenWidth * 0.35;
+    int width25 = screenWidth * 0.1;
 
     int PosXPink = width35;
     int PosXGreen = PosXPink + width25;
@@ -96,49 +77,41 @@ int main() {
     SetTargetFPS(60);
 
     while (!WindowShouldClose()) { // Detect window close button or ESC key 
-        cout << verif_moov.first << endl;
     
         My_Mouse = GetMousePosition();
-        // cout << My_Mouse.x << " " << My_Mouse.y << endl;
-        
         
         if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) ) {
             
             int it = field->getCharacterDataList().size(); 
             
-            if ((My_Mouse.x > PosXPink && My_Mouse.x < PosXPink + Width* 0.15) && (My_Mouse.y > 800  && My_Mouse.y < 800 + Width * 0.15  )) {
+            if ((My_Mouse.x > PosXPink && My_Mouse.x < PosXPink + screenWidth* 0.07) && (My_Mouse.y > 800  && My_Mouse.y < 800 + screenWidth * 0.07  )) {
                 CharacterData* new_Support_perso = new CharacterData(seconde_team,stat_perso,23,44,Support,Support);
                 field->addCharacterData(new_Support_perso);
-                new_Support_perso->setPosition(Position(Width -200,100 + it * 100));
-                //tabcharact = field->getCharacterDataList(); 
+                new_Support_perso->setPosition(Position(screenHeight * 0.775,100 + it * 100));
             }
 
-            if ((My_Mouse.x > PosXGreen && My_Mouse.x < PosXGreen + Width * 0.15) && (My_Mouse.y > 800  && My_Mouse.y < 800 + Width * 0.15  )) {
+            if ((My_Mouse.x > PosXGreen && My_Mouse.x < PosXGreen + screenWidth * 0.07) && (My_Mouse.y > 800  && My_Mouse.y < 800 + screenWidth * 0.07  )) {
                 CharacterData* new_archer_perso = new CharacterData(seconde_team,stat_perso,23,44,Archer,Tank);
                 field->addCharacterData(new_archer_perso);
-                new_archer_perso->setPosition(Position(Width -200,100 + it * 100));
-                //tabcharact = field->getCharacterDataList(); 
+                new_archer_perso->setPosition(Position(screenHeight * 0.775,100 + it * 100));
             }
 
-            if ((My_Mouse.x > PosXRed && My_Mouse.x < PosXRed + Width * 0.15) && (My_Mouse.y > 800  && My_Mouse.y < 800 + Width * 0.15  )) {
+            if ((My_Mouse.x > PosXRed && My_Mouse.x < PosXRed + screenWidth * 0.07) && (My_Mouse.y > 800  && My_Mouse.y < 800 + screenWidth * 0.07  )) {
                 CharacterData* new_Tank_perso = new CharacterData(seconde_team,stat_perso,23,44,Tank,Tank);
                 field->addCharacterData(new_Tank_perso);
-                new_Tank_perso->setPosition(Position(Width -200,100 + it * 100));
-                //tabcharact = field->getCharacterDataList(); 
+                new_Tank_perso->setPosition(Position(screenHeight * 0.775 ,100 + it * 100));
             }
 
-            if ((My_Mouse.x > PosXGray && My_Mouse.x < PosXGray + Width * 0.15) && (My_Mouse.y > 800  && My_Mouse.y < 800 + Width * 0.15  )) {
+            if ((My_Mouse.x > PosXGray && My_Mouse.x < PosXGray + screenWidth * 0.07) && (My_Mouse.y > 800  && My_Mouse.y < 800 + screenWidth * 0.07  )) {
                 CharacterData* new_Sorcerer_perso = new CharacterData(seconde_team,stat_perso,23,44,Sorcerer,Melee);
                 field->addCharacterData(new_Sorcerer_perso);
-                new_Sorcerer_perso->setPosition(Position(Width -200,100 + it * 100));
-                //tabcharact = field->getCharacterDataList(); 
+                new_Sorcerer_perso->setPosition(Position(screenHeight * 0.775,100 + it * 100));
             }
 
-            if ((My_Mouse.x > PosXBlue && My_Mouse.x < PosXBlue + Width * 0.15) && (My_Mouse.y > 800  && My_Mouse.y < 800 + Width * 0.15  )) {
+            if ((My_Mouse.x > PosXBlue && My_Mouse.x < PosXBlue + screenWidth * 0.07) && (My_Mouse.y > 800  && My_Mouse.y < 800 + screenWidth * 0.07  )) {
                 CharacterData* new_Melee_perso = new CharacterData(seconde_team,stat_perso,23,44,Melee,Tank);
                 field->addCharacterData(new_Melee_perso);
-                new_Melee_perso->setPosition(Position(Width -200,100 + it * 100));
-                //tabcharact = field->getCharacterDataList(); 
+                new_Melee_perso->setPosition(Position(screenHeight * 0.775,100 + it * 100));
             }
         }
         
@@ -154,7 +127,6 @@ int main() {
                 if((My_Mouse.x > pos_p.x && My_Mouse.x < pos_p.x + PLAYER_SIZE ) && (My_Mouse.y > pos_p.y && My_Mouse.y < pos_p.y + PLAYER_SIZE)) {
                     verif_moov.first = true;
                     verif_moov.second = tabcharact[i];
-                    //tabcharact[i]->setPosition(Position(My_Mouse.y - PLAYER_SIZE /2,My_Mouse.x - PLAYER_SIZE /2));
                   
                 }  
 
@@ -171,12 +143,6 @@ int main() {
         if(IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) {
             verif_moov.first = false;
         }
-
-        if(field->getCharacterDataList().size() > 0) {
-            // cout << field->getCharacterDataList()[0]->getPosition().x << " - " << field->getCharacterDataList()[0]->getPosition().y << endl;
-        }
-       
-
         BeginDrawing();
         
         ClearBackground(RAYWHITE);
@@ -195,6 +161,9 @@ int main() {
                 switch (le_type) {
                     case Support:
                         DrawRectangle(playerTileX, playerTileY, PLAYER_SIZE, PLAYER_SIZE, PINK);
+                        cout << "player x : " <<playerTileX  << endl;
+                        cout << "player y : " << playerTileY  << endl;
+
                         break;
                     case Tank:
                         DrawRectangle(playerTileX, playerTileY, PLAYER_SIZE, PLAYER_SIZE, RED);
@@ -211,28 +180,26 @@ int main() {
                     default:
                         DrawRectangle(playerTileX, playerTileY, PLAYER_SIZE, PLAYER_SIZE, BLACK);
                 }
-            }else { 
-                playerTileX = playerPosition.x + i*100;
-                playerTileY = playerPosition.y;
-                DrawRectangle(playerTileX, playerTileY, PLAYER_SIZE, PLAYER_SIZE, YELLOW);
             }
             
       
         }
         //Le shop
-        DrawRectangle(PosXPink, 800, Width * 0.15 , Width * 0.15 , PINK);
-        DrawRectangle(PosXGreen, 800, Width * 0.15 , Width * 0.15 , GREEN);
-        DrawRectangle(PosXRed, 800, Width * 0.15 , Width * 0.15 , RED);
-        DrawRectangle(PosXGray, 800, Width * 0.15 , Width * 0.15 , GRAY);
-        DrawRectangle(PosXBlue, 800, Width * 0.15 , Width * 0.15 , BLUE);
+     
+        DrawRectangle(PosXPink, 800, screenWidth * 0.07 , screenWidth * 0.07 , PINK);
+        DrawRectangle(PosXGreen, 800, screenWidth * 0.07 , screenWidth * 0.07 , GREEN);
+        DrawRectangle(PosXRed, 800, screenWidth * 0.07 , screenWidth * 0.07 , RED);
+        DrawRectangle(PosXGray, 800, screenWidth * 0.07 , screenWidth * 0.07 , GRAY);
+        DrawRectangle(PosXBlue, 800, screenWidth * 0.07 , screenWidth * 0.07 , BLUE);
         
-        //void DrawLine(int startPosX, int startPosY, int endPosX, int endPosY, Color color);
+
         //Le Banc
-        DrawLine(0, 750, Width, 750, BLACK );
-        DrawLine(0, 650, Width, 650, BLACK );
+        
+        DrawLine(0, screenHeight * 0.8333, screenWidth, 750, BLACK );
+        DrawLine(0, screenHeight * 0.7222, screenWidth, 650, BLACK );
         
         //delimitation des deux camps enemis
-        DrawLine(0,325, Width, 325, RED);
+        DrawLine(0,screenHeight * 0.3611, screenWidth, 325, RED);
         EndDrawing(); 
     }   
 
