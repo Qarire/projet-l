f5b2b776a621073513c53534d5aa4765d825d13f
theo labrosse <theo.labrosse@etu.univ-lyon1.fr>
2023-11-16 10:53:15 +0100

    correct and finished Action class

--- src/mvc/controller/Action.h
+++ src/mvc/controller/Action.h
@@ -9,16 +9,47 @@ class Action {
 protected:
     
     float time;
+    float cooldownTime;
     float chrono;
-    bool cooldown;
-    
-private:
+    bool isInCooldown;
 
     virtual void Act() = 0;
-    virtual bool void CooldownAction(bool _cooldown) = 0;
+
     float getTime() const { return time; };
     float getChrono() const { return chrono; };
 
-    void Update ();
+public:
+    Action(float time, float cooldownTime = 0) {
+        this->time = time;
+        this->cooldownTime = cooldownTime;
+        this->chrono = 0;
+        this->isInCooldown = false;
+    }
+
+    void Update (float dt) {
+        chrono += dt;
+
+        float tmp = chrono;
+
+        if(!isInCooldown) {
+            while(tmp >= time) {
+                chrono = 0;
+                tmp -= time;
+
+                Act();
+                isInCooldown = true;
+            }
+        }
+        else{
+            if(chrono >= cooldownTime) {
+                chrono = 0
+                isInCooldown = false;
+            }
+        }
+
+
+
+
+    }
     
 };
